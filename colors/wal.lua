e92aace57ffc5d796f8b145c5d56307383743c0c
katawful <katisntgood@gmail.com>
2022-06-16 19:47:58 -0400

    [colors.wal] add example colors file for testing

---
+++ colors/wal.lua
@@ -0,0 +1,19 @@
+
+local colors = {
+    color_0 = "#21140C",
+    color_1 = "#BE4F3F",
+    color_2 = "#768705",
+    color_3 = "#929A0E",
+    color_4 = "#B59863",
+    color_5 = "#D1738A",
+    color_6 = "#7B854D",
+    color_7 = "#FFE1E1",
+}
+
+local opts = {
+    contrast = "soft",
+    colors_name = "wal"
+}
+
+require("kreative.color")["init-colors"](colors)
+require("kreative.main").init(opts)
