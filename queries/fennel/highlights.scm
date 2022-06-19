bddc05c5190a73eea4e797cd761912b5541edcd2
katawful <katisntgood@gmail.com>
2022-06-19 13:24:40 -0400

    [fnl-query] fix broken query

--- queries/fennel/highlights.scm
+++ queries/fennel/highlights.scm
@@ -1,12 +1,20 @@
 ((symbol) @include
- (#any-of? @include
-  "require" "require-macros" "import-macros" "include"
-  ; aniseed
-  "autoload"))
+    (#any-of? @include
+     "require" "require-macros" "import-macros" "include"))
+
 
 ((symbol) @function.macro
- (#any-of? @function.macro
-  "->" "->>" "-?>" "-?>>" "?." "doto" "macro" "macrodebug" "partial" "pick-args"
-  "pick-values" "with-open"
-  ; aniseed
-  "defn" "def" "defn-" "def-" "defonce" "defonce-" "deftest"))
+    (#any-of? @function.macro
+     "->" "->>" "-?>" "-?>>" "?." "doto" "macro" "macrodebug" "partial" "pick-args"
+     "pick-values" "with-open"))
+
+; aniseed
+((symbol) @include
+    (#any-of? @include
+     "autoload")
+    (set! "priority" 1000))
+
+((symbol) @function.macro
+    (#any-of? @function.macro
+     "defn" "def" "defn-" "def-" "defonce" "defonce-" "deftest")
+    (set! "priority" 1000))
