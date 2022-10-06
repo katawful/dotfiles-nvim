560a27a03df8b4fbfe61489ffee9631807e60ffe
Kat <katisntgood@gmail.com>
2022-10-06 15:52:41 -0400

    [queries.fennel] fix for 0.8 requirements

--- queries/fennel/highlights.scm
+++ queries/fennel/highlights.scm
@@ -1,20 +1,25 @@
+;; extends
 ((symbol) @include
-    (#any-of? @include
-     "require" "require-macros" "import-macros" "include"))
+          (#any-of? @include
+           "require" "require-macros" "import-macros" "include"))
 
 
 ((symbol) @function.macro
-    (#any-of? @function.macro
-     "->" "->>" "-?>" "-?>>" "?." "doto" "macro" "macrodebug" "partial" "pick-args"
-     "pick-values" "with-open"))
+          (#any-of? @function.macro
+           "->" "->>" "-?>" "-?>>" "?." "doto" "macro" "macrodebug" "partial" "pick-args"
+           "pick-values" "with-open"))
 
-; aniseed
 ((symbol) @include
-    (#any-of? @include
-     "autoload")
-    (set! "priority" 1000))
+          (#any-of? @include
+           "autoload")
+          (set! "priority" 1000))
 
 ((symbol) @function.macro
-    (#any-of? @function.macro
-     "defn" "def" "defn-" "def-" "defonce" "defonce-" "deftest")
-    (set! "priority" 1000))
+          (#any-of? @function.macro
+           "defn" "def" "defn-" "def-" "defonce" "defonce-" "deftest")
+          (set! "priority" 1000))
+
+; ((list (#any-of? "defn defn-") . (symbol) (symbol) (sequential_table) (string) @string.special))
+; ((list  . (symbol) (symbol) (sequential_table) (string)) @defn (#any-of? @defn "^defn ^defn-")) @string.special
+; ((list . (symbol) (symbol) (sequential_table) (string) @string.special) @defn (#any-of? @defn "^defn"))
+; ((list) @defn ((#any-of? @defn "defn" "def") (list) @text))
